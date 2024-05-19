bb762795dac26312b707c09f92fbfde965fab636
Ekaterina Vititneva <e.vititneva@gmail.com>
2024-05-19 18:03:22 +0200

    Update views.py

--- 07_Testing-CI-CD/airline0/flights/views.py
+++ 07_Testing-CI-CD/airline0/flights/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 
 from . models import Flight, Passenger
