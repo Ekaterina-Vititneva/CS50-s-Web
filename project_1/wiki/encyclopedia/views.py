63bb4e08fdaad5c2440c04675e3ba5eb70e54eab
Ekaterina Vititneva <e.vititneva@gmail.com>
2024-06-19 19:13:23 +0200

    title.html

--- project_1/wiki/encyclopedia/views.py
+++ project_1/wiki/encyclopedia/views.py
@@ -7,4 +7,11 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+    
+def title(request):
+    title = "CSS"
+    return render(request, "encyclopedia/title.html", {
+        "title": title,
+        "title_content": util.get_entry(title)
+    })
 
